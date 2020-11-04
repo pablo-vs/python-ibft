3f9ba4ca7b4d5c49e37c0f5cff1a301ad22001b2
pablo-vs <pavill01@ucm.es>
2020-11-04 20:34:59 +0100

    Removed trash

--- run-offline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-tmux \
-  new-session  "python ibft.py 0 --offline --input-value 'decide this'; read" \; \
-  split-window "python ibft.py 1 --input-value 'decide this'; read" \; \
-  split-window "python ibft.py 2 --input-value 'decide this'; read" \; \
-  split-window "python ibft.py 3 --input-value 'decide this'; read" \; \
-  split-window "bash wait.sh; read" \; \
-  select-layout tiled
